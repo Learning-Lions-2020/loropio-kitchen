d6e8f603f06fc505286770ebf41c7f6558cc3003
Bonface Njuguna <bonfacenjuguna13@gmail.com>
2025-09-03 21:00:14 +0300

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-green-500 text-white p-10 text-3xl">
+      If this is green, Tailwind works!
+    </div>
+  );
+}
+
+export default App
